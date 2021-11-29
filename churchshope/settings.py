2ecc4f1cffe0645f4149f85e3af21789f08ea1eb
jose <kjjose00@gmail.com>
2021-11-29 20:58:32 +0530

    added

--- churchshope/settings.py
+++ churchshope/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1','churchs.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 ROOT_URLCONF = 'churchshope.urls'
 
@@ -120,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
 
 
 # Default primary key field type
--- churchshope/urls.py
+++ churchshope/urls.py
@@ -1,12 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from django.views.static import serve
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app.urls')),
    
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
